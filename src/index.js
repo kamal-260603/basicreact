7f16437ef7a2b72c4df7b6065c73bb8fe8404bc6
Kamal Dasik <kamal@Kamals-MacBook-Air.local>
2026-02-22 16:31:29 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Kamal from './kamal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Kamal />
   </React.StrictMode>
 );
 
---
+++ src/kamal.js
@@ -0,0 +1,9 @@
+function Kamal(){
+    return(
+        <h1>
+            Kamal is great
+        </h1>
+    );
+}
+
+export default Kamal;
